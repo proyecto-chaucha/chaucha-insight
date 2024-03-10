09da700821866a15b599d80a176e4f91115b459c
Camilo <292738+clsource@users.noreply.github.com>
2024-03-10 11:41:46 -0300

    Update start.sh

--- insight/config/start.sh
+++ insight/config/start.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env sh
-# litecore-node.json fails to run bitcoind server (does not pass config)
-# so we have to start it manually before with our config
-/root/insight/chaucha-insight/chauchad -conf=/root/insight/config/bitcoinf.conf
 /root/insight/chaucha-insight/node_modules/litecore-node/bin/litecore-node start
